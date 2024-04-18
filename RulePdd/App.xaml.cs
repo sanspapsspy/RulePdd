f8bd9447c895b5a207c663ddde1d74d7dd67a1b4
sanspapsspy <116632789+sanspapsspy@users.noreply.github.com>
2024-04-18 17:37:15 +0300

    Добавьте файлы проекта.

---
+++ RulePdd/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace RulePdd
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ RulePdd/Windows/PageTabl.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RulePdd.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для PageTabl.xaml
+    /// </summary>
+    public partial class PageTabl : Page
+    {
+        public PageTabl()
+        {
+            InitializeComponent();
+        }
+    }
+}
